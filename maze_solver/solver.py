8f442d6776045b10a86061b30e76ff8ca7e5531e
ice <ice@modernmachin.es>
2017-10-04 13:32:26 +0800

    Add class variables and modify documentation

--- maze_solver/solver.py
+++ maze_solver/solver.py
@@ -1,16 +1,27 @@
 
 class MazeSolver():
-    maze = []
+    maze = None
+    method = None
+
+    def init(self, maze):
+        """
+        Accepts a maze as argument and sets the maze
+        to be solved.
+        """
+        self.maze = maze
 
     def calculate_heuristics(self, method):
         """
         Calculate heuristics of the maze. Accepts
         an argument method.
         """
+        # TODO
         pass
 
     def solve(self):
         """
-        Solves the maze
+        Solves the maze. Throws exception if maze
+        is not set of if method is not set.
         """
+        # TODO
         pass
