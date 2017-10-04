715e2bcd1b6b5df133f49a40279436404a4f1744
ice <ice@modernmachin.es>
2017-10-04 13:10:52 +0800

    Create main file to run maze solver

---
+++ main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import sys
+
+from maze_solver.maze_solver import MazeSolver
+from maze_solver.reader import MazeReader
+
+
+def run(args):
+    maze_paths = args
+
+    for path in maze_paths:
+        reader = MazeReader()
+        maze = reader.read(path)
+
+        solver = MazeSolver()
+        solver.solve(maze)
+
+
+run(sys.argv)
