4039cf37882d292be99b0b7c426ce1edfcc50e9b
ice <ice@modernmachin.es>
2017-10-04 13:28:43 +0800

    Modified documentation

--- maze_solver/reader.py
+++ maze_solver/reader.py
@@ -3,6 +3,7 @@ class MazeReader():
     def read():
         """
         Read .txt file that has a maze inside and
-        returns a maze
+        parses .txt file and returns a maze.
         """
+        # TODO
         pass
