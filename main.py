c749e6f6e71e5c4150d532bbb9fb23510424e30c
Ice Quezon <ice@Thranduil.localdomain>
2017-10-11 21:29:21 +0800

    Modify main.py to show maze after reading and pop first argument

--- main.py
+++ main.py
@@ -8,9 +8,11 @@ from maze_solver.reader import read
 
 def run(args):
     maze_paths = args
+    maze_paths.pop(0)
 
     for path in maze_paths:
         maze = read(path)
+        print(maze)
 
         solver = MazeSolver(maze)
         solver.solve()
