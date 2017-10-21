e304e41da34ee13ac074b00a0aef69643a0a6508
Ice Quezon <ice@Thranduil.localdomain>
2017-10-21 23:52:38 +0800

    Modify main.py to have heuristic method options to run solver

--- main.py
+++ main.py
@@ -2,7 +2,11 @@
 
 import sys
 
-from maze_solver.solver import MazeSolver
+from maze_solver.solver import (
+    MazeSolver,
+    ManhattanHeuristic,
+    StraightHeuristic
+)
 from maze_solver.reader import read
 
 
@@ -10,12 +14,26 @@ def run(args):
     maze_paths = args
     maze_paths.pop(0)
 
+    heuristic_method = ManhattanHeuristic()
+
+    if '-' == maze_paths[0][0]:
+        options = maze_paths.pop(0)
+        if options == '-m':
+            print("Uses Manhattan Heuristic")
+            heuristic_method = ManhattanHeuristic()
+        elif options == '-s':
+            print("Uses Straight Heuristic")
+            heuristic_method = StraightHeuristic()
+        else:
+            print(options, ": is not recognized.")
+            return
+
     for path in maze_paths:
         maze = read(path)
         print("File Name: ", path)
         print(maze)
 
-        solver = MazeSolver(maze)
+        solver = MazeSolver(maze, heuristic_method)
         solver.solve()
         print("=================================================")
 
